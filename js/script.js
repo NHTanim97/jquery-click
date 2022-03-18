18e75fcfdb91e0b1610b241a375553c2e456a9a3
NHTanim97 <101093767+NHTanim97@users.noreply.github.com>
2022-03-18 12:14:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    // sudo two class hide hobe;
+   $('button').click(function(){
+    $('.two').hide();
+   });
+
+
+
+//    all hide hobe sudo btn cara;
+   $('#btn').click(function(){
+     $('.one').hide();
+     $('h1').hide();
+   });
+
+
+
+//    btn shoho hide hobe;
+   $("#btn2").click(function(){
+    $('*').hide();
+   }); 
+
+
+
+
+   // 
+   $("#num").click(function(){
+     $('h2').hide();
+   });
+
+
+});
+
+
+
